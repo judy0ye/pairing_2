db658a7f7c5cea9c229bdcd251795ae5cb391612
Judy Ye <judyy.ye@gmail.com>
2023-04-19 13:31:46 -0700

    add plant functionality

--- plants.js
+++ plants.js
@@ -1,7 +1,11 @@
 var gets_sunshine = true;
-var ounces_of_water = 10;
+var ounces_of_water = 5;
 if (ounces_of_water >= 10 && gets_sunshine) {
     console.log("Your plant will thrive")
+} else if(ounces_of_water >= 10 && !gets_sunshine) {
+    console.log("Your plant needs sunshine")
+} else if(gets_sunshine) {
+    console.log("Your plant needs water")
 } else {
     console.log("Your plant has died")
 }
