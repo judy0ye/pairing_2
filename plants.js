10b353161ffc30f79ec5da230acb20727e3ed63e
Judy Ye <judyy.ye@gmail.com>
2023-04-19 13:23:00 -0700

    Initial commit

---
+++ plants.js
@@ -0,0 +1,7 @@
+var gets_sunshine = true;
+var ounces_of_water = 10;
+if (ounces_of_water >= 10 && gets_sunshine) {
+    console.log("Your plant will thrive")
+} else {
+    console.log("Your plant has died")
+}
